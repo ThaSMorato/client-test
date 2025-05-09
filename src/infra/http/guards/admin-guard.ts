9df576540c4ee6766c3b7895b8d0529add1aaa7d
ThaSMorato <tha.s.morato@gmail.com>
2025-05-09 08:49:32 -0300

    feat: :sparkles: adiciona guards de autenticação AdminGuard e JwtGuard para validação de usuários e controle de acesso em rotas protegidas

---
+++ src/infra/http/guards/admin-guard.ts
@@ -0,0 +1,46 @@
+import type { Request, Response } from 'express'
+
+import type { UserDAO } from '@/common/data/user-dao.interface'
+import { diContainer } from '@/infra/container'
+import { AUTH_SYMBOLS } from '@/infra/container/auth/symbols'
+
+export function AdminGuard(
+  _: unknown,
+  __: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalMethod = descriptor.value
+
+  descriptor.value = async function (request: Request, response: Response) {
+    const userId = request.body.userId
+
+    if (!userId) {
+      return response.status(401).json({
+        message: 'Unauthorized',
+        error: 'UnauthorizedError',
+      })
+    }
+
+    try {
+      const userDao = diContainer.get<UserDAO>(AUTH_SYMBOLS.UserDAO)
+
+      const adminExists = await userDao.doesAdminExist(userId)
+
+      if (!adminExists) {
+        return response.status(401).json({
+          message: 'Unauthorized',
+          error: 'UnauthorizedError',
+        })
+      }
+
+      return originalMethod.apply(this, [request, response])
+    } catch (error) {
+      return response.status(401).json({
+        message: 'Unauthorized',
+        error: 'UnauthorizedError',
+      })
+    }
+  }
+
+  return descriptor
+}
