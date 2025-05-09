8fdb53b9dd43e460f05976ac60f4598212e3cee8
ThaSMorato <tha.s.morato@gmail.com>
2025-05-09 08:18:06 -0300

    feat: :sparkles: adiciona a classe AxiosProductGateway para integração com a API de produtos e atualiza o esquema de ambiente para incluir PRODUCT_API_URL

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -3,6 +3,10 @@ import { z } from 'zod'
 export const envSchema = z.object({
   JWT_SECRET: z.string(),
   DATABASE_URL: z.string(),
+  PRODUCT_API_URL: z
+    .string()
+    .url()
+    .default('https://fakestoreapi.com/products'),
 })
 
 export type Env = z.infer<typeof envSchema>
---
+++ src/infra/gateways/axios/product/axios-product-gateway.ts
@@ -0,0 +1,68 @@
+import type { AxiosInstance } from 'axios'
+import axios from 'axios'
+import { inject, injectable } from 'inversify'
+
+import type {
+  ProductDTO,
+  ProductGateway,
+} from '@/client/enterprise/gateways/product-gateway.interface'
+import { COMMON_SYMBOLS } from '@/infra/container/infra/symbols'
+import type { EnvService } from '@/infra/env/env-service'
+
+type ProductApiResponse = {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating?: {
+    rate: number
+    count: number
+  }
+}
+
+@injectable()
+export class AxiosProductGateway implements ProductGateway {
+  private readonly axiosInstance: AxiosInstance
+
+  constructor(
+    @inject(COMMON_SYMBOLS.EnvService) private readonly envService: EnvService,
+  ) {
+    this.axiosInstance = axios.create({
+      baseURL: this.envService.get('PRODUCT_API_URL'),
+    })
+  }
+
+  async findById(id: string): Promise<ProductDTO | null> {
+    const response = await this.axiosInstance.get<ProductApiResponse | ''>(
+      `/${id}`,
+    )
+
+    if (response.data === '') {
+      return null
+    }
+
+    return {
+      id: response.data.id.toString(),
+      title: response.data.title,
+      price: response.data.price,
+      imageUrl: response.data.image,
+      reviewScore: response.data.rating?.rate,
+      reviewCount: response.data.rating?.count,
+    }
+  }
+
+  async getProducts(): Promise<ProductDTO[]> {
+    const response = await this.axiosInstance.get<ProductApiResponse[]>('/')
+
+    return response.data.map((product) => ({
+      id: product.id.toString(),
+      title: product.title,
+      price: product.price,
+      imageUrl: product.image,
+      reviewScore: product.rating?.rate,
+      reviewCount: product.rating?.count,
+    }))
+  }
+}
